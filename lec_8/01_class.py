985a040b5c82238e5b1c95f3bdd42b225a884ec5
kvazargius <novitskiybronislav@yandex.ru>
2021-04-09 17:18:34 +0300

    Доработка игры "ПУШКА"

--- lec_8/01_class.py
+++ lec_8/01_class.py
@@ -1,14 +1,44 @@
+class Dragon:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+
+
+    def is_alive(self):
+        return  self.health > 0
+
+
+    def get_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+
+
+    def talk(self):
+        print(self.name, " health", self.health, 'Hit, me!')
+
+
+    def final_cry(self):
+        print('Ты победил дракона ', self.name)
 
 
 def main():
-    health = 100
+    dragon = Dragon('Smog')
+    enemy_list = [Dragon('Smog'), Dragon('Hidra')]
+
     finish = False
     while not finish:
-        print("My health", health, 'Hit, me!')
+        enemy = enemy_list[0]
+        enemy.talk()
         damage = int(input())
-        health -= damage
-        if health <= 0:
+        enemy.get_damage(damage)
+
+        if not enemy.is_alive():  #  удалить из списка мёртвого врага
+            enemy.final_cry()
+            enemy_list.pop(0)
+        if not enemy_list:  #  Проверить пуст ли список врагов
             finish = True
+
 
     print('Ты победил!!!')
 
